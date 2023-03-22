530cb8c263b8a2016983fdfb0fae7d5ec3fdd71f
defiminds <defiminds@onionmail.org>
2023-03-22 05:41:23 +0000

    build

--- js/protect.js
+++ js/protect.js
@@ -16,3 +16,53 @@
         // Block ability to select all
         e.preventDefault();
     });
+
+    document.addEventListener("mousedown", function(event) {
+        if (event.button === 0 && !event.ctrlKey) {
+        event.preventDefault();
+    }
+    });
+
+    document.addEventListener('mousedown', function(event) {
+        if (event.shiftKey) {
+        event.preventDefault();
+    }
+    });
+
+  // desativa a seleção de texto na página
+  function disableSelection() {
+    if (typeof document.onselectstart!="undefined") {
+      document.onselectstart=new Function("return false");
+    }
+    else {
+      document.onmousedown=new Function("return false");
+      document.onmouseup=new Function("return true");
+    }
+  }
+
+  // ativa a seleção de texto na página
+  function enableSelection() {
+    if (typeof document.onselectstart!="undefined") {
+      document.onselectstart=new Function("return true");
+    }
+    else {
+      document.onmousedown=null;
+      document.onmouseup=null;
+    }
+  }
+
+  // desativa a seleção de texto quando o mouse é arrastado
+  function blockSelectAll() {
+    disableSelection();
+    document.addEventListener("mousemove", disableSelection);
+  }
+
+  // ativa a seleção de texto quando o mouse é solto
+  function unblockSelectAll() {
+    enableSelection();
+    document.removeEventListener("mousemove", disableSelection);
+  }
+
+  // adiciona os listeners aos eventos de mouse
+  document.addEventListener("mousedown", blockSelectAll);
+  document.addEventListener("mouseup", unblockSelectAll);
